1712d1268cde0f2a16b54fbfd56fce455c1c8eae
vincentarck <73167671+vincentarck@users.noreply.github.com>
2022-02-08 15:20:37 +0700

    Answer

---
+++ interview.js
@@ -0,0 +1,13 @@
+function gedung(noLoker){
+	let baseLoker = 5;
+	let cariLoker = 0;
+	let lantai = 0, i=0;
+	while(i<noLoker){
+		i+=baseLoker
+		baseLoker>6?baseLoker=5:baseLoker+=1
+		lantai++;
+	}
+	return lantai
+}
+console.log(gedung(19))
+console.log(gedung(1000))
